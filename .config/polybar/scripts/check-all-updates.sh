09905037f4c5d72bd6889ef9cabcc548a1542ff8
IlmastroStefanuzzo <ilmastrostefanuzzo@gmail.com>
2022-07-19 00:04:59 +0200

    Fixed updates script

--- .config/polybar/scripts/check-all-updates.sh
+++ .config/polybar/scripts/check-all-updates.sh
@@ -1,20 +1,4 @@
-#!/bin/sh
-#source https://github.com/x70b1/polybar-scripts
+#!/bin/bash
 
-#if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
-#    updates_arch=0
-#fi
-
-# if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
-if ! updates=$((yay -Qu 2> /dev/null && pacman -Qu 2> /dev/null) | wc -l); then
-    updates=-1
-fi
-
-#updates=$(("$updates_arch" + "$updates_aur"))
-
-if [ "$updates" -gt -1 ]; then
-    echo "$updates"
-else
-    echo "N/D"
-fi
-
+updates="$({ yay -Qu && pacman -Qu; } 2>/dev/null)"
+[[ -z $updates ]] && printf "N/D" || wc -l <<< "$updates"
